459ed6698f6d74c3f846f6f2230ba73753117ce2
Nimavat Mansi <144536348+NimavatMansi-19@users.noreply.github.com>
2024-07-10 22:30:48 +0530

    Add files via upload

---
+++ Data Struture/lab 7/Poliish_Notation_Postfix.java
@@ -0,0 +1,164 @@
+import java.util.*;
+import java.lang.*;
+class Methods{
+    int top=0;
+      String str="(a+b^c^d)*(e+f/d))";
+    int max=str.length();
+    char[] stack=new char[max];
+   // stack[0]=str.charAt(0);
+    public void Push(char x){
+        if(top>=max){
+            System.out.println("Stack OverFlow...");
+        }else{
+            top++;
+            stack[top]=x;
+        }
+        displaystack();
+    }
+    public char Pop(){
+        if(top<=-1){
+            System.out.println("Stack overflow...");
+            return(0);
+        }
+        else{
+            top--;
+            return(stack[top+1]);
+        }
+    }
+    public void displaystack(){
+        for(int i=0;i<=top;i++){
+        System.out.println(stack[i]);
+        }
+        
+    }
+    static int i=0;
+   public  char NextChar() {
+    
+    char next=str.charAt(i);
+    if(i<max){
+        next=str.charAt(i);    
+        i++;  
+    }
+    return next; 
+    }
+    public int Input_Precedence(int n) {
+        int f=0;
+        if(n=='+' || n=='-'){
+            f=1;
+        }if(n=='*' || n=='/'){
+            f=3;
+        }
+        if(n=='^'){
+            f=6;
+        }
+        if(n>='a' || n<='z'){
+            f=7;
+        }
+        if(n=='('){
+            f=9;
+        }
+        if(n==')' ){
+            f=0;
+        }
+
+        return f;
+        
+    }
+    public int Stack_Precedence(int n) {
+        int g=0;
+        if(n=='+' || n=='-'){
+            g=2;
+        }if(n=='*' || n=='/'){
+            g=4;
+        }
+        if(n=='^'){
+            g=5;
+        }
+        if(n>='a' || n<='z'){
+            g=8;
+        }
+        if(n=='('){
+            g=0;
+        }
+        if(n==')' ){
+            g=0;
+        }
+
+        return g;
+        
+    }
+    public int Rank( char temp) {
+        int r=0;
+        switch (temp) {
+            case '+':
+                r=-1;
+                break;
+                case '-':
+                r=-1;
+                break;
+                case '*':
+                r=-1;
+                break;
+                case '/':
+                r=-1;
+                break;
+                case '^':
+                r=-1;
+                break;
+                case '(':
+                r=0;
+                break;
+                case ')':
+                r=0;
+                break;
+            default:
+            r=1;
+                break;
+        } 
+        return r;
+        
+    }
+}
+
+public class Poliish_Notation_Postfix {
+    public static void main(String[] args) {
+        Methods m=new Methods();
+        String Polish="";
+        int Ranks=0;
+        m.stack[0]='(';
+        char next=m.NextChar();
+        char temp;
+        System.out.println(next);
+        while (next!='\0'){
+            if(m.top<-1){
+                System.out.println("Invalid Strng>>>>");
+                return;
+            }
+            while (m.Stack_Precedence(m.stack[m.top])>m.Input_Precedence(next)){
+                temp=m.Pop();
+                Polish=Polish+temp;
+                Ranks=Ranks+m.Rank(temp);
+             
+             if(Ranks<1){
+                System.out.println("Invalidsss");
+                return;
+             }
+             if(m.Stack_Precedence(m.stack[m.top])!=m.Input_Precedence(next)){
+                m.Push(next);
+             }else{
+                m.Pop();
+             }
+            
+             next=m.NextChar();
+            } 
+        }
+        System.out.println(m.top);
+        if(m.top!=0 || Ranks!=1 ){
+            System.out.println("Invalid");
+        }
+        else{
+            System.out.println("Valid");
+        }
+        System.out.println(Polish);
+            }
+}
